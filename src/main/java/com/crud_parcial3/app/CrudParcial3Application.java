a5354c1db566bb0b4855bacfb9a7507b2f04d2b2
manuelcastro2 <malavermanuelricardo@gmail.com>
2023-06-05 17:41:10 -0500

    EMPESANDO PARCIAL3_JAVA

---
+++ src/main/java/com/crud_parcial3/app/CrudParcial3Application.java
@@ -0,0 +1,13 @@
+package com.crud_parcial3.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudParcial3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudParcial3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud_parcial3/app/CrudParcial3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud_parcial3.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudParcial3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
